ef47ea5b635a582ff2a30b61bd739ea9f11af980
bethfreitas <bethjanuario.freitas@gmail.com>
2026-03-04 18:45:31 -0300

    Delete retangulo.py

--- retangulo.py
+++
@@ -1,7 +0,0 @@
-largura = int(input("Digite a largura do retangulo (maximo 20) "))
-altura = int(input("Digite a altura do retangulo (maximo 20) "))
-if altura > 20:
-    print("Valor invalido")
-else :
-    for i in range (altura):
-        print("*" * largura)

03d24579823e5eeeca083038b3ccb256e5340ea0
bethfreitas <bethjanuario.freitas@gmail.com>
2026-03-04 18:46:28 -0300

    Delete trianguloInvertido.py

--- trianguloInvertido.py
+++
@@ -1,7 +0,0 @@
-tamanho = int(input("Digite o tamanho(maximo 20) "))
-if tamanho > 20:
-    print("Valor invalido")
-else:
-    for i in range(tamanho):
-        espaços = " " * i
-        print(espaços + "*" * (tamanho - i))
